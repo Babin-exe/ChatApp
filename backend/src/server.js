0732487c7ada513b5a2d44856213a701deac5e16
Babin-exe <nepalbabin4@gmail.com>
2025-10-16 16:57:42 +0545

    Initial setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 4000;
-app.listen(port, () => {console.log(`The server is running at port :${port}`)});
+app.listen(port, () => {
+  console.log(`The server is running at port :${port}`);
+});
