753391cd8684b5bbcba350e99303053cfe7bf057
Babin-exe <nepalbabin4@gmail.com>
2026-05-26 22:38:22 +0545

    more checks in multer middleware filter

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -18,7 +18,7 @@ const fileFilter = (req, file, cb) => {
 
 export const uploadImage = multer({
     storage,
-    limits: { fileSize: MAX_FILE_SIZE, files: 1 },
+    limits: { fileSize: MAX_FILE_SIZE, files: 1, fields: 1, fieldSize: 4 * 1024, parts: 2 },
     fileFilter
 }).single("image");
 
